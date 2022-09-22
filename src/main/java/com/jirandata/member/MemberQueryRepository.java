d79774706ec827ac887b0898cc28225f2572a109
chillyunn <dkwhkp0@naver.com>
2022-09-22 13:18:29 +0900

    동적쿼리 예시 추가

--- src/main/java/com/jirandata/member/MemberQueryRepository.java
+++ src/main/java/com/jirandata/member/MemberQueryRepository.java
@@ -1,8 +1,10 @@
 package com.jirandata.member;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 
 import java.util.List;
@@ -23,5 +25,15 @@ public class MemberQueryRepository {
         return queryFactory.selectFrom(member)
                 .where(member.name.contains(name))
                 .fetch();
+    }
+    public List<Member> findByAllColumns(String name,String id){
+        return queryFactory.selectFrom(member)
+                .where(containsMemberId(id))
+                .fetch();
+    }
+    private BooleanExpression containsMemberId(String id){
+        if(!StringUtils.hasLength(id))
+            return null;
+        return member.memberId.contains(id);
     }
 }

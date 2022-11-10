4b58abed20d027163c0b370bb1d9c53093bb6de5
chillyunn <dkwhkp0@naver.com>
2022-11-10 10:45:52 +0900

    공통 column 정의

---
+++ src/main/java/com/jirandata/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package com.jirandata;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+    @CreatedDate @Column(name = "created_datetime")
+    private LocalDateTime createdDateTime;
+
+    @LastModifiedDate @Column(name = "last_modified_datetime")
+    private LocalDateTime lastModifiedDateTime;
+}

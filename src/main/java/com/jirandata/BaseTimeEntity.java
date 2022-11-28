4d8bc2b06336b33f741c40fa7b4e2161b8cf6fe3
chillyunn <dkwhkp0@naver.com>
2022-11-28 11:31:59 +0900

    LocalDateTime 형식변경을 위한 타입변경

--- src/main/java/com/jirandata/BaseTimeEntity.java
+++ src/main/java/com/jirandata/BaseTimeEntity.java
@@ -3,20 +3,31 @@ package com.jirandata;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
-import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
-    @CreatedDate @Column(name = "created_datetime")
-    private LocalDateTime createdDateTime;
+    @CreatedDate
+    private String createdAt;
 
-    @LastModifiedDate @Column(name = "last_modified_datetime")
-    private LocalDateTime lastModifiedDateTime;
+    @LastModifiedDate
+    @Column(name = "modifiedAt")
+    private String modifiedAt;
+
+    @PrePersist
+    void onPrePersist(){
+        this.createdAt= LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+        this.modifiedAt= this.createdAt;
+    }
+    @PreUpdate
+    public void onPreUpdate(){
+        this.modifiedAt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+    }
 }

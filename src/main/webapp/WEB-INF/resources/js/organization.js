4a032b4b203eec8e922a442b96c0cedbd8916651
chillyunn <dkwhkp0@naver.com>
2022-12-07 17:48:36 +0900

    View 추가 / 수정

--- src/main/webapp/WEB-INF/resources/js/organization.js
+++ src/main/webapp/WEB-INF/resources/js/organization.js
@@ -1,52 +1,284 @@
-$(function (){
+$(function () {
     initializeGroupTree();
     initializeAgentTable();
     addGroupTreeEventListener();
     addAgentTableEventListener();
+    $('a[data-bs-target="#organization-tab-pane"]').on('shown.bs.tab', function () {
+        $('#policyTable').DataTable().destroy();
+    });
+    $('a[data-bs-target="#policy-tab-pane"]').on('shown.bs.tab', function () {
+        $('#policyTable').DataTable({
+            columnDefs:[{
+                orderable: false,
+                className: 'select-checkbox',
+                targets: 0,
+                defaultContent: ''
+            }],
+            select:{
+                style: 'os',
+                selector: 'td:first-child'
+            },
+            order: [[1,'asc']],
+            serverSide: true,
+            processing: true,
+            searching: false,
+            ajax: {
+                url: 'api/policies',
+                type: 'POST',
+                data: function (d) {
+                    d.searchType = "",
+                        d.searchValue = ""
+                }
+            },
+            columns:[
+                {data: null},
+                {data: 'id'},
+                {data: 'name'},
+                {data: 'content'},
+                {data: 'createdAt'},
+                {data: 'modifiedAt'},
+            ],
+            initComplete:function(){
+                $('#checkAll').prop("checked",false);
+                $('#checkAll').click(function(){
+                    if($(this).prop("checked")){
+                        $('#policyTable').DataTable().rows().select();
+                    }
+                    else{
+                        $('#policyTable').DataTable().rows().deselect();
+                    }
+                })
+            },
+            rowId: function(a){
+                return a.id;
+            }
+    });
+    $('#btnShowPolicyCreateModal').on('click', function () {
+        $("#policyModal").modal('show');
+        $("#inputPolicyName").val('');
+        $('#policyContent').val('');
+
+        $("#groupNameTable").DataTable({
+            searching: false,
+            ordering: false,
+            info: false,
+            paging: false,
+            destroy: true,
+            columns: [
+                {data: 'name'}
+            ]
+        })
+        $("#groupNameTable").DataTable().clear().draw();
+        $('#groupNameTable tbody').on('click', 'tr', function () {
+            if ($(this).hasClass('selected')) {
+                $(this).removeClass('selected');
+            } else {
+                $("#groupNameTable").DataTable().$('tr.selected').removeClass('selected');
+                $(this).addClass('selected');
+            }
+        });
+        $('#btnDeselectGroup').click(function () {
+            $("#groupNameTable").DataTable().row('.selected').remove().draw(false);
+        });
+        $("#agentNameTable").DataTable({
+            searching: false,
+            ordering: false,
+            info: false,
+            paging: false,
+            destroy: true,
+            columns: [
+                {data: 'name'}
+            ]
+        })
+        $("#agentNameTable").DataTable().clear().draw();
+        $('#agentNameTable tbody').on('click', 'tr', function () {
+            if ($(this).hasClass('selected')) {
+                $(this).removeClass('selected');
+            } else {
+                $("#agentNameTable").DataTable().$('tr.selected').removeClass('selected');
+                $(this).addClass('selected');
+            }
+        });
+        $('#btnDeselectAgent').click(function () {
+            $("#agentNameTable").DataTable().row('.selected').remove().draw(false);
+        });
+        $("#policyModalLabel").html("정책 추가");
+
+    })
+    $('#btnShowGroupTreeModal').on('click', function () {
+        $('#policyTargetSelectModal').modal('show');
+        $('#agentSelectTableWrapper').css('display', 'none');
+        $('#groupSelectTree').css('display', 'block');
+        $('#groupSelectTree').jstree({
+            core: {
+                data: getGroupList()
+            },
+        })
+    })
+
+    $('#btnShowAgentTableModal').on('click', function () {
+        $('#policyTargetSelectModal').modal('show');
+        $('#agentSelectTableWrapper').css('display', 'block');
+        //$('#groupSelectTree').jstree('destroy');
+        $('#groupSelectTree').css('display', 'none');
+        $('#agentSelectTable').DataTable({
+            serverSide: true,
+            processing: true,
+            searching: false,
+            destroy: true,
+            ajax: {
+                url: 'api/agents',
+                type: 'POST',
+                data: function (d) {
+                    d.searchType = "",
+                        d.searchValue = "",
+                        d.group = getSelectedNodeName() ? getSelectedNodeName() : null
+                }
+            },
+            columns: [
+                {data: 'name'},
+                {data: 'groupName'},
+            ]
+        });
+        $('#agentSelectTable tbody').on('click', 'tr', function () {
+            $('#policyTargetSelectModal').modal('hide');
+            $('#agentSelectTable tbody').off('click');
+            const data = $('#agentSelectTable').DataTable().row(this).data();
+            $('#agentNameTable').DataTable().row.add({
+                "name": data.name
+            }).draw();
+
+        });
+    });
+
+});
+$('#btnDeletePolicy').on('click',function(){
+    const ids = $('#policyTable').DataTable().rows('.selected').ids().toArray();
+    $.ajax({
+        type: "DELETE",
+        contentType: 'application/json',
+        url: "/api/policies",
+        data: ids,
+        error: function (e) {
+            console.log(e);
+        },
+        success: function () {
+        }
+    })
 })
-function getGroupList(){
+$('#groupSelectTree').on("select_node.jstree", function (e, data) {
+    $('#policyTargetSelectModal').modal('hide');
+
+    $('#groupNameTable').DataTable().row.add({
+        "name": data.node.text
+    }).draw();
+});
+
+$('#btnConfirmCreatePolicy').on('click', function () {
+    const groups = [];
+    const agents = [];
+    for (const i of $('#groupNameTable').DataTable().rows().data().toArray()) {
+        groups.push(i.name);
+    }
+    for (const i of $('#agentNameTable').DataTable().rows().data().toArray()) {
+        agents.push(i.name);
+    }
+    const policy = JSON.stringify({
+        name: $("#inputPolicyName").val(),
+        content: $('#policyContent').val(),
+        groups: groups,
+        agents: agents,
+    });
+    $.ajax({
+        type: "POST",
+        contentType: 'application/json',
+        url: "/api/policy",
+        data: policy,
+        error: function (e) {
+            console.log(e);
+        },
+        success: function () {
+            refreshGroupTree();
+        }
+    })
+});
+$('#agentNameTable').on('click', function () {
+    if ($(this).hasClass('selected')) {
+        $(this).removeClass('selected');
+    } else {
+        $('#agentNameTable').DataTable().$('tr.selected').removeClass('selected');
+        $(this).addClass('selected');
+    }
+})
+$('#groupNameTable').on('click', function () {
+    if ($(this).hasClass('selected')) {
+        $(this).removeClass('selected');
+    } else {
+        $('#groupNameTable').DataTable().$('tr.selected').removeClass('selected');
+        $(this).addClass('selected');
+    }
+})
+$('#btnDeselectGroup').on('click', function () {
+    if ($('#groupNameTable').hasClass('selected')) {
+        $('#groupNameTable').DataTable().row('.selected').remove().draw(false);
+    }
+})
+$('#btnDeselectAgent').on('click', function () {
+    if ($('#agentNameTable').hasClass('selected')) {
+        $('#agentNameTable').DataTable().row('.selected').remove().draw(false);
+    }
+})
+})
+;
+
+function getGroupList() {
     let result;
     $.ajax({
         type: "GET",
-        url:"api/groups",
+        url: "api/groups",
         dataType: "json",
-        async:false,
-        timeout:30000,
-        error : function (request,status,error){
-            console.log("error"+error);
+        async: false,
+        timeout: 30000,
+        error: function (request, status, error) {
+            console.log("error" + error);
         },
-        success: function (data){
-            result= changeParentValueToHash(data);
+        success: function (data) {
+            result = changeParentValueToHash(data);
         }
     });
     return result;
 }
-function initializeGroupTree(){
-    const groupTree=$('#groupTree').jstree({
-        core:{
-            data:getGroupList()
+
+function initializeGroupTree() {
+    const groupTree = $('#groupTree').jstree({
+        core: {
+            data: getGroupList()
         },
     })
-    groupTree.bind("select_node.jstree",function(event,data){
+    groupTree.bind("select_node.jstree", function (event, data) {
+        // if (getSelectedNodeId() === data.node.id){
+        //     data.instance.deselect_node(data.node);
+        // }
         $('#agentTable').DataTable().destroy();
         initializeAgentTable();
     })
 }
+
 function initializeAgentTable() {
     $('#agentTable').DataTable({
-        serverSide:true,
-        processing:true,
+        serverSide: true,
+        processing: true,
         searching: false,
-        ajax:{
+        ajax: {
             url: 'api/agents',
             type: 'POST',
-            data: function (d){
+            data: function (d) {
                 d.searchType = "",
-                    d.searchValue="",
-                    d.group=getSelectedNodeName() ? getSelectedNodeName() : null
+                    d.searchValue = "",
+                    d.group = getSelectedNodeName() ? getSelectedNodeName() : null
             }
         },
-        columns:[
+        columns: [
             {data: 'name'},
             {data: 'groupName'},
             {data: 'ip'},
@@ -56,52 +288,55 @@ function initializeAgentTable() {
         ]
     });
 }
+
 function refreshGroupTree() {
     $('#groupTree').jstree(true).settings.core.data = getGroupList();
     $('#groupTree').jstree(true).refresh();
 }
-function refreshAgentTable(){
+
+function refreshAgentTable() {
     $('#agentTable').DataTable().ajax.reload();
 }
+
 function createGroup() {
-    const group=JSON.stringify({
-        name:$("#inputGroupName").val(),
-        parentName:$("#inputParentGroupName").val(),
-        sort:$("#inputSortValue").val()
+    const group = JSON.stringify({
+        name: $("#inputGroupName").val(),
+        parentName: $("#inputParentGroupName").val(),
+        sort: $("#inputSortValue").val()
     });
     $.ajax({
-        type:"POST",
+        type: "POST",
         contentType: 'application/json',
-        url:"/api/group",
-        data:group,
-        error:function(e){
+        url: "/api/group",
+        data: group,
+        error: function (e) {
             console.log(e);
         },
-        success: function(){
+        success: function () {
+            refreshGroupTree();
+        }
+    })
+}
+function updateGroup(id) {
+    const group = JSON.stringify({
+        name: $("#inputGroupName").val(),
+        parentName: $("#inputParentGroupName").val(),
+        sort: $("#inputSortValue").val()
+    });
+    $.ajax({
+        type: "PUT",
+        contentType: 'application/json',
+        url: "/api/group/" + id,
+        data: group,
+        error: function (e) {
+            console.log(e);
+        },
+        success: function () {
             refreshGroupTree();
         }
     })
 }
 
-function updateGroup(id) {
-    const group=JSON.stringify({
-        name:$("#inputGroupName").val(),
-        parentName:$("#inputParentGroupName").val(),
-        sort:$("#inputSortValue").val()
-    });
-    $.ajax({
-        type:"PUT",
-        contentType: 'application/json',
-        url:"/api/group/"+id,
-        data:group,
-        error:function(e){
-            console.log(e);
-        },
-        success: function(){
-            refreshGroupTree();
-        }
-    })
-}
 function initializeGroupCreationModal() {
     showGroupModalAndChangeLabel("그룹 추가");
     $("#groupModal").find('input[type=text]').each(function () {
@@ -121,6 +356,7 @@ function initializeGroupUpdateModal() {
 
     $("#btnConfirmCreation")?.attr('id', 'btnConfirmUpdate');
 }
+
 function initializeAgentCreationModal() {
     showAgentModalAndChangeLabel("에이전트 추가")
     $("#agentModal").find('input[type=text]').each(function () {
@@ -129,6 +365,7 @@ function initializeAgentCreationModal() {
     $("#inputAgentGroupName").val(getSelectedNodeName());
     $("#btnConfirmUpdateAgent")?.attr('id', 'btnConfirmCreateAgent');
 }
+
 function initializeAgentUpdateModal() {
     showAgentModalAndChangeLabel("에이전트 수정");
     const row = $("#agentTable").DataTable().row('.selected').data();
@@ -138,48 +375,54 @@ function initializeAgentUpdateModal() {
     $("#inputMacValue").val(row?.mac);
     $("#btnConfirmCreateAgent")?.attr('id', 'btnConfirmUpdateAgent');
 }
+
 function getSelectedNodeId() {
     return $('#groupTree').jstree('get_selected', true)[0]?.id;
 }
-function getSelectedNodeName(){
+
+function getSelectedNodeName() {
     return $('#groupTree').jstree('get_selected', true)[0]?.text;
 }
-function getSelectedNodeParentName(){
+
+function getSelectedNodeParentName() {
     const parentId = $('#groupTree').jstree('get_selected', true)[0].parent
 
     return $('#groupTree').jstree(true).get_node(parentId)?.text;
 }
-function getSelectedNodeSort(){
+
+function getSelectedNodeSort() {
     return $('#groupTree').jstree('get_selected', true)[0]?.original.sort;
 }
 
-function deleteGroup(){
-    const id= getSelectedNodeId();
+function deleteGroup() {
+    const id = getSelectedNodeId();
     $.ajax({
-        type:"DELETE",
+        type: "DELETE",
         contentType: 'application/json',
-        url:"/api/group/"+id,
-        error:function(e){
+        url: "/api/group/" + id,
+        error: function (e) {
             console.log(e);
         },
-        success: function(){
+        success: function () {
             refreshGroupTree();
         }
     })
 }
-function deleteAgent(id){
+
+function deleteAgent(id) {
     $.ajax({
-        type:"DELETE",
+        type: "DELETE",
         contentType: 'application/json',
-        url:"/api/agent/"+id,
-        error:function(e){
+        url: "/api/agent/" + id,
+        error: function (e) {
             console.log(e);
         },
-        success: function(){
+        success: function () {
             refreshAgentTable();
         }
     })
 }
+
 function changeParentValueToHash(data) {
     for (let i in data) {
         if (data[i].parent == null) {
@@ -188,14 +431,17 @@ function changeParentValueToHash(data) {
     }
     return data;
 }
+
 function showGroupModalAndChangeLabel(label) {
     $("#groupModal").modal('show');
     $("#groupModalLabel").html(label)
 }
-function showAgentModalAndChangeLabel(label){
+
+function showAgentModalAndChangeLabel(label) {
     $("#agentModal").modal('show');
     $("#agentModalLabel").html(label);
 }
+
 function addGroupTreeEventListener() {
     $('#btnShowGroupCreationModal').on('click', function () {
         initializeGroupCreationModal();
@@ -213,46 +459,48 @@ function addGroupTreeEventListener() {
             updateGroup(id);
         })
     });
-    $('#btnDeleteGroup').on('click',function (){
+    $('#btnDeleteGroup').on('click', function () {
         deleteGroup();
     });
 }
-function createAgent(){
-    const agent= JSON.stringify({
-        name:$("#inputAgentName").val(),
-        groupName:$("#inputAgentGroupName").val(),
-        ip:$("#inputIpValue").val(),
-        mac:$("#inputMacValue").val()
+
+function createAgent() {
+    const agent = JSON.stringify({
+        name: $("#inputAgentName").val(),
+        groupName: $("#inputAgentGroupName").val(),
+        ip: $("#inputIpValue").val(),
+        mac: $("#inputMacValue").val()
     });
     $.ajax({
-        type:"POST",
+        type: "POST",
         contentType: 'application/json',
-        url:"api/agent",
-        data:agent,
-        error:function(e){
+        url: "api/agent",
+        data: agent,
+        error: function (e) {
             console.log(e);
         },
-        success: function (){
+        success: function () {
             refreshAgentTable();
         }
     })
 }
+
 function updateAgent(id) {
     const agent = JSON.stringify({
-        name:$("#inputAgentName").val(),
-        groupName:$("#inputAgentGroupName").val(),
-        ip:$("#inputIpValue").val(),
-        mac:$("#inputMacValue").val()
+        name: $("#inputAgentName").val(),
+        groupName: $("#inputAgentGroupName").val(),
+        ip: $("#inputIpValue").val(),
+        mac: $("#inputMacValue").val()
     });
     $.ajax({
-        type:"PUT",
+        type: "PUT",
         contentType: 'application/json',
-        url:"api/agent/"+id,
-        data:agent,
-        error:function(e){
+        url: "api/agent/" + id,
+        data: agent,
+        error: function (e) {
             console.log(e);
         },
-        success: function (){
+        success: function () {
             refreshAgentTable();
         }
     })
@@ -279,7 +527,7 @@ function addAgentTableEventListener() {
             updateAgent(getSelectedRowId());
         });
     });
-    $('#btnDeleteAgent').on('click',function(){
+    $('#btnDeleteAgent').on('click', function () {
         deleteAgent(getSelectedRowId());
     })
     $('#agentTable tbody').on('click', 'tr', function () {
@@ -291,3 +539,4 @@ function addAgentTableEventListener() {
         }
     });
 }
+
--- src/main/webapp/WEB-INF/views/organization.jsp
+++ src/main/webapp/WEB-INF/views/organization.jsp
@@ -12,6 +12,7 @@
     <%--    CSS     --%>
     <link rel="stylesheet" href="plugins/bootstrap-5.2.0/bootstrap.min.css">
     <link rel="stylesheet" href="plugins/dataTables-1.12.1/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="plugins/dataTables-1.12.1/select.dataTables.min.css">
     <link rel="stylesheet" href="plugins/jstree-3.3.12/style.min.css">
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/organization.css">
@@ -22,8 +23,10 @@
     <script defer src="plugins/jquery-3.6.1/jquery-3.6.1.min.js"></script>
     <script defer src="plugins/jquery-3.6.1/jquery.dataTables.min.js"></script>
     <script defer src="plugins/dataTables-1.12.1/dataTables.bootstrap5.min.js"></script>
+    <script defer src="plugins/dataTables-1.12.1/dataTables.select.min.js"></script>
     <script defer src="plugins/bootstrap-5.2.0/bootstrap.min.js"></script>
     <script defer src="plugins/jstree-3.3.12/jstree.min.js"></script>
+    <script defer src="plugins/icheck-1.0.3/icheck.min.js"></script>
     <script defer src="js/organization.js"></script>
     <link rel="icon" href="images/favicon.ico" type="image/x-icon">
 </head>
@@ -94,6 +97,91 @@
         </div>
     </div>
 </div>
+<%--Policy Modal--%>
+<div class="modal fade" id="policyModal" tabindex="-1" aria-labelledby="policyModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="policyModalLabel"></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="policy-name-wrapper">
+                    <h5>정책명 </h5>
+                    <input type="text" class="form-control" id="inputPolicyName" aria-describedby="policyName">
+                </div>
+                <div class="policy-target-wrapper">
+                    <h5>대상</h5>
+                    <div class="policy-modal-content">
+                        <div class="groupname-table-wrapper">
+                            <button id="btnShowGroupTreeModal" type="button" class="btn btn-primary btn-sm">
+                                <i class="fa-solid fa-folder-plus"></i> 추가
+                            </button>
+                            <button id="btnDeselectGroup" type="button" class="btn btn-danger btn-sm">
+                                <i class="fa-solid fa-folder-minus"></i> 삭제
+                            </button>
+                            <table id="groupNameTable" class="table table-striped">
+                                <thead>
+                                <tr>
+                                    <th>그룹</th>
+                                </tr>
+                                </thead>
+                            </table>
+                        </div>
+                        <div class="agentname-table-wrapper">
+                            <button id="btnShowAgentTableModal" type="button" class="btn btn-primary btn-sm">
+                                <i class="fa-solid fa-user-plus"></i> 추가
+                            </button>
+                            <button id="btnDeselectAgent" type="button" class="btn btn-danger btn-sm">
+                                <i class="fa-solid fa-user-xmark"></i> 삭제
+                            </button>
+                            <table id="agentNameTable" class="table table-striped">
+                                <thead>
+                                <tr>
+                                    <th>에이전트</th>
+                                </tr>
+                                </thead>
+                            </table>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="policy-content">
+                    <h5>정책 내용</h5>
+                    <textarea class="form-control" id="policyContent"  rows="3"></textarea>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">취소</button>
+                <button type="button" id="btnConfirmCreatePolicy" class="btn btn-primary" data-bs-dismiss="modal">확인</button>
+            </div>
+        </div>
+    </div>
+</div>
+<%--Modal-in-PolicyModal--%>
+<div class="modal fade" id="policyTargetSelectModal" tabindex="-1" aria-labelledby="groupSelectModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="groupSelectModalLabel">그룹 선택</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body" id="groupSelectTree"></div>
+            <div class="modal-body" id="agentSelectTableWrapper">
+                <table id="agentSelectTable" class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th>이름</th>
+                        <th>그룹</th>
+                    </tr>
+                    </thead>
+                </table>
+            </div>
+            <div class="modal-footer">
+            </div>
+        </div>
+    </div>
+</div>
 <%--Contents--%>
 <div class="tap-wrap">
     <!-- Nav tabs -->
@@ -117,13 +205,13 @@
                     <h4 class="content-title">조직도</h4>
                     <div class="organization-modify-wrap">
                         <button id="btnShowGroupCreationModal" type="button" class="btn btn-primary btn-sm">
-                            <i class="fa-solid fa-user-plus"></i> 추가
+                            <i class="fa-solid fa-folder-plus"></i> 추가
                         </button>
                         <button id="btnShowGroupUpdateModal" type="button" class="btn btn-secondary btn-sm">
-                            <i class="fa-solid fa-pencil"></i> 수정
+                            <i class="fa-solid fa-folder-open"></i> 수정
                         </button>
                         <button id="btnDeleteGroup" type="button" class="btn btn-danger btn-sm">
-                            <i class="fa-solid fa-user-xmark"></i> 삭제
+                            <i class="fa-solid fa-folder-minus"></i> 삭제
                         </button>
                     </div>
                 </div>
@@ -168,7 +256,35 @@
 
         </div>
         <div class="tab-pane container fade" id="policy-tab-pane" role="tabpanel" aria-labelledby="policy-tab"
-             tabindex="0">menu1
+             tabindex="0">
+<div class="policy-wrap">
+    <h4 class="content-title">정책 목록</h4>
+    <div class="policy-control-wrap">
+        <button id="btnShowPolicyCreateModal" type="button" class="btn btn-primary btn-sm" >
+            <i class="fa-solid fa-file-signature"></i> 추가
+        </button>
+        <button id="btnShowPolicyUpdateModal" type="button" class="btn btn-secondary btn-sm">
+            <i class="fa-solid fa-wrench"></i> 수정
+        </button>
+        <button id="btnDeletePolicy" type="button" class="btn btn-danger btn-sm">
+            <i class="fa-solid fa-trash-can"></i> 삭제
+        </button>
+    </div>
+    <div class="table-wrap" >
+        <table id="policyTable" class="table table-striped">
+            <thead>
+            <tr>
+                <th id="checkboxCol"><input type="checkbox" name="checkAll" id="checkAll"></th>
+                <th>정책명</th>
+                <th>적용중인 그룹 수</th>
+                <th>적용중인 에이전트 수</th>
+                <th>최초생성일</th>
+                <th>최근변경일</th>
+            </tr>
+            </thead>
+        </table>
+    </div>
+</div>
         </div>
     </div>
 
